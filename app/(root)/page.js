3d3689755777cb140e3de59f6f19b0f66ad4af74
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2023-08-15 22:56:05 +0530

    about page fixing

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import { currentUser } from '@clerk/nextjs'
-// import About from '@/components/About'
+import About from '@/temp/pages/About'
 
 export default async function Home() {
-  const user = await currentUser();
-  if (user) { console.log(user) };
+ 
   return (
     <>
       <h1>Home page</h1>
+      {/* <About/> */}
       
     </>
   )
